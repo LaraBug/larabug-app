162209990f4c96bd7fd1b93e778a9e269c2bc448
Sebastiaan Kloos <sebastiaan@codecycler.com>
2022-02-24 18:50:21 +0100

    WIP

---
+++ app/Http/Controllers/IssuesController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Issue;
+use Illuminate\Http\Request;
+
+class IssuesController extends Controller
+{
+    public function index()
+    {
+        $issues = Issue::whereHas('project', function ($q) {
+            $q->where('user_id', auth()->user()->id);
+        })
+            ->filter(request()->only('search'))
+            ->lastest('last_exception_at')
+            ->paginate();
+
+        return inertia('Issues/Index', [
+            'filters' => request()->only('search'),
+            'issues' => $issues,
+        ]);
+    }
+
+    public function show(Request $request, $id)
+    {
+        $issue = Issue::find($id);
+
+        abort_unless($issue->project->user->is(auth()->user()), 403);
+
+        $exception = $issue->exceptions()
+            ->orderBy('created_at', 'DESC')
+            ->first();
+
+        $exceptions = $issue->exceptions()
+            ->filter($request->only('search', 'status', 'has_feedback'))
+            ->withCount('feedback')
+            ->latest()
+            ->paginate(10);
+
+        return inertia('Issues/Show', [
+            'issue' => $issue,
+            'exception' => $exception,
+            'exceptions' => $exceptions,
+            'project' => $issue->project,
+        ]);
+    }
+}
--- resources/assets/js/panel/Layouts/AppLayout.vue
+++ resources/assets/js/panel/Layouts/AppLayout.vue
@@ -61,6 +61,28 @@
                     Projects
                 </NavListItem>
 
+                <NavListItem @click="open = false" href="/panel/projects"
+                             :is-active="route().current('panel.projects.*')" :isopen="open">
+                    <template #icon>
+                        <svg
+                            class="w-6 h-6 text-primary-400"
+                            fill="none"
+                            stroke="currentColor"
+                            viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"
+                            ></path>
+                        </svg>
+                    </template>
+
+                    Issues
+                </NavListItem>
+
                 <NavListItem @click="open = false" href="/panel/profile" :is-active="route().current('panel.profile.*')"
                              :isopen="open">
                     <template #icon>
